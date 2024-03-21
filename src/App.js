7c2aa28efc795ed74f6fb0df7a076ee0fa9a2cc2
Copeland Carter <smartycope@gmail.com>
2024-03-20 23:03:29 -0600

    Added some styling

--- src/App.js
+++ src/App.js
@@ -25,21 +25,27 @@ function EZRegex({reset=null, children}) {
 
     return (
         <div ref={drag} className="ezregex">
-            <DropEZRegex side={true}/>
-            {children}
-            <DropEZRegex side={true}/>
+            <DropEZRegex side={true}>
+                <img src='left.svg' className='side' alt=''/>
+            </DropEZRegex>
+
+            <div className='ezregex-inner'>
+                {children}
+            </div>
+
+            <DropEZRegex side={true}>
+                <img src='right.svg' className='side' alt=''/>
+            </DropEZRegex>
         </div>
     )
 }
 
-
-function DropEZRegex({side=false}) {
-    const defaultNested = <div className='default-drop-ezregex'>{side ? '+' : null}</div>
-    const [nested, setNested] = useState(defaultNested)
+function DropEZRegex({children=<div className='default-drop-ezregex'/>, side=false}) {
+    const [nested, setNested] = useState(children)
     const [{ canDrop, isOver}, drop] = useDrop(() => ({
         accept: 'ezregex',
         drop: (item, monitor) => {
-            setNested(<EZRegex reset={() => setNested(defaultNested)}> {item.children} </EZRegex>)
+            setNested(<EZRegex reset={() => setNested(children)}> {item.children} </EZRegex>)
         },
         collect: (monitor) => ({
             isOver: monitor.isOver(),
@@ -50,7 +56,7 @@ function DropEZRegex({side=false}) {
     if (side){
         return (
             <div ref={drop} className='drop-ezregex-side' style={{
-                backgroundColor: isOver ? (canDrop ? 'green' : 'red') : 'lightgreen',
+                // backgroundColor: isOver ? (canDrop ? 'green' : 'red') : 'lightgreen',
             }}>
                 {nested}
             </div>
@@ -66,8 +72,6 @@ function DropEZRegex({side=false}) {
     }
 }
 
-
-
 function MatchAmt(){
     return (
         <EZRegex>
@@ -78,13 +82,11 @@ function MatchAmt(){
     )
 }
 
-
 function Compiler(){
     return (
         <DropEZRegex/>
     )
 }
-
 
 
 function App() {
@@ -96,46 +98,7 @@ function App() {
                 <MatchAmt/>
                 <br/>
                 <Compiler/>
-                {/* <svg width="200" height="100">
-
-                    <path fill="none" stroke="black" strokeWidth="2" d="m321.62162,94.05405l0,75.13514l36.21622,36.21622l0,89.18919l-31.35135,30.81081l0,94.05405l-27.02703,0l0,-326.48649l22.16216,1.08108z"/>
-                    <text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle">Your text here</text>
-                </svg>
-                <svg width="400" height="500" scale={2}>
-
-                    <path d="m321.62162,94.05405l0,75.13514l36.21622,36.21622l0,89.18919l-31.35135,30.81081l0,94.05405l-27.02703,0l0,-326.48649l22.16216,1.08108z" opacity="NaN" stroke="#000" fill="none"/>
-                </svg> */}
-                <div className='svgs'>
-                    {/* <text x="30%" y="30%" dominantBaseline="middle" textAnchor="middle">Your text here</text> */}
-                    <svg width="200" height="100" scale={20}>
-                        <polyline points="5,0 5,5 0,10 0,20 5,25 5,30" fill="none" stroke="black" />
-                    </svg>
-                    <p>text here!</p>
-                    <svg width="200" height="100">
-                        <polyline points="0,0 0,5 5,10 5,20 0,25 0,30" fill="none" stroke="black" />
-                    </svg>
-                </div>
-                <svg
-                    width="10mm"
-                    height="60mm"
-                    viewBox="0 0 20.022602 60"
-                    version="1.1"
-                    id="svg1"
-                    xmlSpace='preserve'
-                    xmlns="http://www.w3.org/2000/svg"
-                    xm
-                    // xmlns:svg="http://www.w3.org/2000/svg"
-                >
-                <defs/>
-                    <g
-                        id="layer1"><path
-                        style={{fill: "#24a18e"}}//, fillOpacity:1, strokeWidth:0.264583}}
-                        d="M 10,3.6191812 V 10 c 0,0 9.979818,6.164543 10,10 0.03546,6.739233 0.0243,13.080826 0,20 -0.01312,3.735962 -10,10 -10,10 v 6.380819 A 3.6191812,3.6191812 135 0 1 6.3808188,60 H 0 V 0 H 6.3808188 A 3.6191812,3.6191812 45 0 1 10,3.6191812 Z"
-                        id="path11" />
-                    </g>
-                </svg>
             </DndProvider>
-
         </div>
     );
 }

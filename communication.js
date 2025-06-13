ec4952a2d0937adc0747d53c5331a9fd4b300389
Copeland Carter <smartycope@gmail.com>
2025-06-13 09:05:53 -0500

    Updates

--- communication.js
+++
@@ -1,6 +0,0 @@
-const py2js = document.querySelector('#py2js')
-
-export function send_py2js(signal, data){
-    const evt = new CustomEvent('custom', {'detail': [signal, data]})
-    py2js.dispatchEvent(evt)
-}
